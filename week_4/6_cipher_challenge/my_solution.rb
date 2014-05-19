f40d1f69cb87ed8c6a24ca4ae4ef77578a41f38e
Ismail <ismailkalimi@gmail.com>
2014-05-19 18:14:15 -0500

    Finished Cipher Challenge

--- week_4/6_cipher_challenge/my_solution.rb
+++ week_4/6_cipher_challenge/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W4: Cipher Challenge
 
 
-# I worked on this challenge with: .
+# I worked on this challenge with: By Myself.
 
 
 
@@ -73,18 +73,47 @@ end
 
 # Your Refactored Solution
 
+def north_korean_cipher(coded_message)
+  shift = 4
+  input = coded_message.downcase.split("")
+  decoded_message = []
 
+  cipher = Hash[(('a'..'z').to_a.rotate(shift)).zip('a'..'z').to_a]
+
+  white_space = Hash.new
+  ("@#$%^&*").split('').to_a.each { |key| white_space[key] = " " }
+  cipher.merge!(white_space)
+
+  numbers = {}
+  (0..9).to_s.split('').to_a.each { |key| numbers[key] = key }
+  cipher.merge!(numbers)
+
+  input.each { |i|
+    cipher.include?(i) ? decoded_message << cipher[i] : decoded_message << i
+  }
+  decoded_message = decoded_message.join('')
+  decoded_message.gsub!(/\d+/) { |num| num.to_i / 100 }
+  decoded_message
+end
 
 
 
 # Driver Code:
 p north_korean_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver code and should print true
 # Find out what Kim Jong Un is saying below and turn it into driver code as well. Driver Code statements should always return "true"
-p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.")
-p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!")
-p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.")
-p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!")
-p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?")
+p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.") == "our people eat the most delicious and nutritious foods from our 10000 profitable farms."
+p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!") == "our nukes are held together by grape-flavored toffee. don't tell the us!"
+p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.") == "if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman."
+p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!") == "next stop: south korea, then japan, then the world!"
+p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?") == "can somebody just get me 100 bags of cool ranch doritos?"
 
 # Reflection
+# It was interesting doing this challenge…sadly I couldn’t find a partner in time to pair for this challenge, 
+# but it was fun trying to figure out how to refractor the code as efficiently as possible, I first tried using 
+# the ruby regex to find the special, white space and number characters, but felt it was sloppy because i was already
+# implementing a Hash for the cipher, so for better readability  and understudying, I thought I’ll keep using the 
+# hash that way if someone else reads my code they can figure out that I kept comparing each character with the hash 
+# key and then retrieved the corresponding value, i wish i could streamlined the adding the special characters and the 
+# numbers into the hash though.
+
  
