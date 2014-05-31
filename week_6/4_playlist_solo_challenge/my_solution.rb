caccf2af25cd55a17869c0c293ddafc668dc8b1e
Ismail <ismailkalimi@gmail.com>
2014-05-31 14:39:50 -0500

    Added Solo challenge

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -6,79 +6,52 @@
 
 # Pseudocode
 # DEFINE CLASS Song
-	METHOD initialize(2 PARAMETERS: <title>(String Value), <artist>(String Value))
+	# METHOD initialize(2 PARAMETERS: <title>(String Value), <artist>(String Value))
+	# initialize instance variables title
+	# initialize instance variables artist
+	# END METHOD
+	# METHOD title
+	# 	RETURN instance title
+	# END METHOD
+	# METHOD artist
+	# 	RETURN instance artist
+	# END METHOD
+	# METHOD play
+	# 	print "#{@title} by #{@artist} is playing" 
+	# END METHOD
 
-# END METHOD
+# END CLASS
+
+# DEFINE CLASS Playlist INHERITS CLASS Song
+# 	METHOD initialize('n' NUMBER OF PARAMETER(s): <list>(Object Type))
+# 	initialize instance variables title
+# 	initialize instance variables artist
+# 	END METHOD
+# 	METHOD title
+# 		RETURN instance title
+# 	END METHOD
+# 	METHOD artist
+# 		RETURN instance artist
+# 	END METHOD
+# 	METHOD play
+# 		print "#{@title} by #{@artist} is playing" 
+# 	END METHOD
+
+# END CLASS
 
 
 
 # Initial Solution
-class Song
-	def initialize(title, artist)
-		@title = title
-		@artist = artist
-	end
-	def title
-		@title
-	end
-	def artist
-		@artist
-	end
-	def play
-		p "#{@title} by #{@artist} is playing"
-	end
-end
-
-class Playlist < Song
-	def initialize(*list)
-		@play_list = []
-		list.each{ |song|
-			@play_list << song
-		}
-	end
-
-	def add(*songs_to_add)
-		songs_to_add.each {|song|
-			@play_list << song
-		}
-	end
-
-	def num_of_tracks
-		@play_list.count
-	end
-
-
-	def remove(song_to_remove)
-		@play_list.delete(song_to_remove)
-	end
-
-	def includes?(check_song)
-		@play_list.include?(check_song)
-	end
-
-	def play_all
-		@play_list.each{ |song|
-			song.play
-		}
-	end
-
-	def display
-		@play_list.each {|song|
-			p "#{song.title} by #{song.artist}"
-		}
-	end
-
-
-end
-
-
-# Refactored Solution
 # class Song
-# 	attr_reader :title, :artist
-
 # 	def initialize(title, artist)
 # 		@title = title
 # 		@artist = artist
+# 	end
+# 	def title
+# 		@title
+# 	end
+# 	def artist
+# 		@artist
 # 	end
 # 	def play
 # 		p "#{@title} by #{@artist} is playing"
@@ -95,8 +68,8 @@ end
 
 # 	def add(*songs_to_add)
 # 		songs_to_add.each {|song|
-# 		@play_list << song
-# 	}
+# 			@play_list << song
+# 		}
 # 	end
 
 # 	def num_of_tracks
@@ -123,8 +96,61 @@ end
 # 			p "#{song.title} by #{song.artist}"
 # 		}
 # 	end
-
 # end
+
+
+# Refactored Solution
+
+
+class Song
+	attr_reader :title, :artist
+
+	def initialize(title, artist)
+		@title = title
+		@artist = artist
+	end
+	def play
+		p "#{@title} by #{@artist} is playing"
+	end
+end
+
+class Playlist < Song
+	def initialize(*list)
+		@list = list
+	end
+
+	def add(*songs_to_add)
+		songs_to_add.each {|song|
+		@list << song
+	}
+	end
+
+	def num_of_tracks
+		@list.count
+	end
+
+
+	def remove(song_to_remove)
+		@list.delete(song_to_remove)
+	end
+
+	def includes?(check_song)
+		@list.include?(check_song)
+	end
+
+	def play_all
+		@list.each{ |song|
+			song.play
+		}
+	end
+
+	def display
+		@list.each {|song|
+			p "#{song.title} by #{song.artist}"
+		}
+	end
+
+end
 
 
 
@@ -154,3 +180,12 @@ my_playlist.display
 
 
 # Reflection
+# I learned a lot from this challenge especially how to inherit classes
+# in ruby, at first I had implemented a rudimentary display and play method
+# in the playlist class but after reading a bit more about inheritance 
+# in Ruby I figured out how I can implement the previous class methods in 
+# the Playlist class.
+
+# I also learned a lot about the attribute reader, attribute writer, and 
+# attribute accessor and how they work, using the attributes definitely minimizes
+# the lines to write.
--- week_6/8_BONUS_CarClass/my_solution.rb
+++ week_6/8_BONUS_CarClass/my_solution.rb
@@ -9,6 +9,36 @@
 
 
 # 3. Initial Solution
+class Car
+	attr_reader :model, :color, :total_distance, :speed, :speed_limit, :distance
+	def initialize(model, color)
+		@model = model
+		@color = color
+		@total_distance = 0
+		@speed = 0 
+
+	end
+
+	def drive(distance, speed_limit)
+		@speed_limit = speed_limit
+		@total_distance += distance
+		if @speed < @speed_limit
+			accelerate
+		elsif @speed > @speed_limit
+			decelerate
+		else
+			@speed = @speed_limit
+		end
+	end
+
+	def accelerate
+		@speed = @speed_limit
+	end
+	def decelerate
+		@speed = @speed_limit
+	end
+
+end
 
 
 
@@ -21,6 +51,13 @@
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+dodge = Car.new("Dodge","Red")
+dodge.drive(0.25, 25)
+dodge.drive(1.5, 25)
+dodge.drive(2, 5)
+dodge.total_distance
+
+
 
 
 
