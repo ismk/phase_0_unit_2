d5aac7ea60dac0723fdedf1f542a302ec8a7656d
Ismail <ismailkalimi@gmail.com>
2014-06-01 12:12:52 -0500

    Refactoring Sol

--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -1,26 +1,78 @@
 # U2.W6: Refactoring for Code Readability
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # Original Solution
+=begin
 
+class CreditCard
+	def initialize(number)
+		@number = number
+		raise ArgumentError.new("Not a Credit Card Number") if @number.to_s.length != 16
+	end
+
+	def check_card
+		@number = @number.to_s.split('').map { |digit| digit.to_i }
+
+		(@number.length).times {|i|
+			if i % 2==0
+				@number[i] += @number[i]
+			end
+		}
+
+		@number = @number.map {|i|
+			i.to_s.length > 1 ? i.to_s.split('').map{|x| x.to_i} : i
+		}
+
+		@number.flatten!.inject(:+) % 10 == 0
+	end
+end
+
+=end
 
 
 
 # Refactored Solution
+class CreditCard
+	def initialize(number)
+		@number = number
+		raise ArgumentError.new("Not a Credit Card Number") unless @number.to_s.length == 16 #changed to unless to make it more Ruby-esque
+	end
+
+	def check_card
+		@number = @number.to_s.split('').map { |digit| digit.to_i }
+
+		(0...16).step(2) {|i| @number[i] += @number[i] }
+
+		@number.map! {|i| i.size > 1 ? i.to_s.split('').map(&:to_i) : i }.flatten!.inject(:+) % 10 == 0 #learned about &: and made it into one line
+	end
+
+end
 
 
-
-
+def assert
+	raise ArgumentError.new ("INVALID INVALID INVALID") unless yield
+end
 
 
 # DRIVER TESTS GO BELOW THIS LINE
+valid_number = 4563960122001999
+credit = CreditCard.new(valid_number)
+assert { credit.check_card() }
+
+invalid_number = ("1"*16).to_i
+credit = CreditCard.new(invalid_number)
+assert { credit.check_card() }
 
 
 
 
 
+# Reflection
+=begin
+Well I looked into other solutions and more than refactoring theirs, I found out how I could 
+improve mine, so thats what I did, incorporated whatever I found useful and tried to better my code.	
+=end
 
-# Reflection 
